4a0265d83239e353a76446551e45273f97976e72
vinh <vinhpt72@gmail.com>
2019-12-26 18:11:58 +0700

    m

---
+++ header.php
@@ -0,0 +1,23 @@
+<header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img  class = "logo"src="images/logo.png">
+                </div>
+                <div class="col-md-6"> 
+                    <div class="login_logout">
+                         <i class="fa fa-sign-in" style="font-size: 18px;"><a class = "sign" href="#">Đăng nhập</a></i>     
+                         <i class="fa fa-sign-out" style="font-size: 18px;"><a class="sign" href="#">Đăng kí</a></i> 
+                    </div c>
+                   
+                    <form class="form-inline " >
+                        <input class="form-control mr-sm-2" type="text" /> 
+                        <button class="btn btn-primary my-2 my-sm-0" type="submit">
+                            Tìm kiếm
+                        </button>
+                    </form>    
+                </div>
+                </div>
+            </div>
+        </div>
+    </header>
---
+++ login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style11.css">
+    <link rel="stylesheet" href="stylelogin.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <?php
+    include "header.php";
+    ?>
+    <main>
+    <div class="container">
+	    <div class="row">
+	     	<div class="col-md-6">
+          <form action="/action_page.php">
+            <div class="form-group">
+                <label for="email">Email address:</label>
+                <input type="email" class="form-control" placeholder="Enter email" id="email">
+            </div>
+            <div class="form-group">
+                <label for="pwd">Password:</label>
+                <input type="password" class="form-control" placeholder="Enter password" id="pwd">
+            </div>
+            <div class="form-group form-check">
+              <label class="form-check-label">
+                <input class="form-check-input" type="checkbox"> Remember me
+              </label>
+            </div>
+            <div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+              <button type="submit" class="btn btn-primary1">Cancel</button>
+            </div>     
+          </form>    
+		</div>
+		<div class="col-md-6">
+		</div>
+	</div>
+</div>
+    </main>
+</html>
